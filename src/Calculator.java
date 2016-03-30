4ad85020ad2a2229accfb72dd2b484181b34fef3
Lemon Jenkins <lem0000000000n@gmail.com>
2016-03-31 00:29:43 +0300

    The output of the error message

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,16 +1,16 @@
 public class Calculator {
-        public int add(int a, int b){
-            return a + b;
+        public int add(int number1, int number2){
+            return number1 + number2;
         }
-        public int subtract (int a, int b){
-            return a - b;
+        public int subtract (int number1, int number2){
+            return number1 - number2;
         }
-        public int multiply (int a, int b){
-            return a * b;
+        public int multiply (int a, int number2){
+            return a * number2;
         }
-        public int divide (int a, int b){
-            return a / b;
+        public int divide (int number1, int number2){
+            return number1 / number2;
         }
-        public int degree(int a, int b) {
-            return (int) Math.pow(a, b);}
-}
+        public int degree(int number1, int number2){
+            return (int) Math.pow(number1, number2);}
+                        }
--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,52 @@
 import java.util.Scanner;
 
 public class Main {
-
+    private static final String ENTER_EXPRESSION_TEXT = "Enter the calculation expression: ";
+    private static final String fuckYOU = "Fuck YOU, enter normal expression!";
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int a,b;
-        String d;
+        int number1, number2;
+        String inputSign;
+        number1=0;
+        number2=0;
+        inputSign=" ";
         Calculator c = new Calculator();
-        System.out.printf("Enter the calculation expression: ");
-        a=in.nextInt();
-        d=in.next();
-        b=in.nextInt();
-
-        if("+".equals(d)){
-            System.out.print(a + " + " + b + " = " + c.add(a,b));
+        System.out.printf(ENTER_EXPRESSION_TEXT);
+        if (in.hasNextInt()) {
+            number1 = in.nextInt();
         }
-        if("-".equals(d)){
-            System.out.print(a + " - " + b + " = " + c.subtract(a,b));
+        else {
+            System.out.println(fuckYOU);
+            System.exit(1);
         }
-        if("*".equals(d)){
-           System.out.print(a + " * " + b + " = " + c.multiply(a,b));
+        inputSign = in.next();
+             if ("+".equals(inputSign) || "-".equals(inputSign) || "*".equals(inputSign) ||
+                    "/".equals(inputSign) || "^".equals(inputSign)) {
+                if (in.hasNextInt()) {
+                    number2 = in.nextInt();}
+                else {
+                    System.out.println(fuckYOU);
+                    System.exit(3);
+                }
+            }
+            else {
+                System.out.println(fuckYOU);
+                System.exit(2);
+            }
+        if ("+".equals(inputSign)) {
+            System.out.print(number1 + " + " + number2 + " = " + c.add(number1, number2));
         }
-        if("/".equals(d)){
-            System.out.print(a + " / " + b + " = " + c.divide(a,b));
+        if ("-".equals(inputSign)) {
+            System.out.print(number1 + " - " + number2 + " = " + c.subtract(number1, number2));
         }
-        if("^".equals(d)){
-            System.out.print(a + "^" + b + " = " + c.degree(a,b));
+        if ("*".equals(inputSign)) {
+            System.out.print(number1 + " * " + number2 + " = " + c.multiply(number1, number2));
+        }
+        if ("/".equals(inputSign)) {
+            System.out.print(number1 + " / " + number2 + " = " + c.divide(number1, number2));
+        }
+        if ("^".equals(inputSign)) {
+            System.out.print(number1 + "^" + number2 + " = " + c.degree(number1, number2));
         }
     }
 
