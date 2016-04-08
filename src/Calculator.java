c577b88b6e4d26751287945a7bcf4200b98c882c
Lemon Jenkins <lem0000000000n@gmail.com>
2016-04-08 23:38:36 +0300

    Rewritten class calculator Added check function: reading, verification, singVerification Fixed application logic Check for correct input

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,16 +1,78 @@
-public class Calculator {
-        public int add(int number1, int number2){
-            return number1 + number2;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class Calculator {
+    private String[] expression = new String[3];
+    private int[] numbers = {0, 0};
+    private static final String TEXT_INCORECT_EXP = "!!!!!You entered an incorrect expression!!!!!!!";
+
+    void start() {
+        reading();
+        verification(expression);
+        signVerification(expression[1]);
+        if ("+".equals(expression[1])) {
+            System.out.print(numbers[0] + expression[1] + numbers[1] + " = " + add(numbers[0], numbers[1]));
         }
-        public int subtract (int number1, int number2){
-            return number1 - number2;
+        if ("-".equals(expression[1])) {
+            System.out.print(numbers[0] + expression[1] + numbers[1] + " = " + subtract(numbers[0], numbers[1]));
         }
-        public int multiply (int a, int number2){
-            return a * number2;
+        if ("*".equals(expression[1])) {
+            System.out.print(numbers[0] + expression[1] + numbers[1] + " = " + multiply(numbers[0], numbers[1]));
         }
-        public int divide (int number1, int number2){
-            return number1 / number2;
+        if ("/".equals(expression[1])) {
+            System.out.print(numbers[0] + expression[1] + numbers[1] + " = " + divide(numbers[0], numbers[1]));
         }
-        public int degree(int number1, int number2){
-            return (int) Math.pow(number1, number2);}
-                        }
+        if ("^".equals(expression[1])) {
+            System.out.print(numbers[0] + expression[1] + numbers[1] + " = " + degree(numbers[0], numbers[1]));
+        }
+    }
+
+    private void reading() {
+        Scanner expressionElement = new Scanner(System.in);
+        expression = expressionElement.nextLine().split(" ");
+    }
+
+    private void verification(String[] expForValidity) {
+
+                try {
+                    numbers[0] = Integer.parseInt(expForValidity[0]);
+                    if (expForValidity.length > 2) {
+                        numbers[1] = Integer.parseInt(expForValidity[2]);
+                    }
+                    }
+                    catch (NumberFormatException e) {
+                    System.out.println(TEXT_INCORECT_EXP);
+                    throw new RuntimeException();
+                }
+            }
+
+    private void signVerification(String signIn) {
+        Pattern p = Pattern.compile("^\\+|\\-|\\*|/|\\^$");
+        Matcher m = p.matcher(signIn);
+        if (!m.matches()) {
+            System.out.println(TEXT_INCORECT_EXP);
+            System.exit(53);
+        }
+    }
+
+    private int add(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    private int subtract(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    private int multiply(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
+    private int divide(int firstNumber, int secondNumber) {
+        return firstNumber / secondNumber;
+    }
+
+    private int degree(int firstNumber, int secondNumber) {
+        return (int) Math.pow(firstNumber, secondNumber);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,9 @@
-import java.util.Scanner;
 
 public class Main {
     private static final String ENTER_EXPRESSION_TEXT = "Enter the calculation expression: ";
-    private static final String fuckYOU = "Fuck YOU, enter normal expression!";
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int number1, number2;
-        String inputSign;
-        number1=0;
-        number2=0;
-        inputSign=" ";
-        Calculator c = new Calculator();
+        Calculator calculator = new Calculator();
         System.out.printf(ENTER_EXPRESSION_TEXT);
-        if (in.hasNextInt()) {
-            number1 = in.nextInt();
-        }
-        else {
-            System.out.println(fuckYOU);
-            System.exit(1);
-        }
-        inputSign = in.next();
-             if ("+".equals(inputSign) || "-".equals(inputSign) || "*".equals(inputSign) ||
-                    "/".equals(inputSign) || "^".equals(inputSign)) {
-                if (in.hasNextInt()) {
-                    number2 = in.nextInt();}
-                else {
-                    System.out.println(fuckYOU);
-                    System.exit(3);
-                }
-            }
-            else {
-                System.out.println(fuckYOU);
-                System.exit(2);
-            }
-        if ("+".equals(inputSign)) {
-            System.out.print(number1 + " + " + number2 + " = " + c.add(number1, number2));
-        }
-        if ("-".equals(inputSign)) {
-            System.out.print(number1 + " - " + number2 + " = " + c.subtract(number1, number2));
-        }
-        if ("*".equals(inputSign)) {
-            System.out.print(number1 + " * " + number2 + " = " + c.multiply(number1, number2));
-        }
-        if ("/".equals(inputSign)) {
-            System.out.print(number1 + " / " + number2 + " = " + c.divide(number1, number2));
-        }
-        if ("^".equals(inputSign)) {
-            System.out.print(number1 + "^" + number2 + " = " + c.degree(number1, number2));
-        }
+        calculator.start();
     }
-
 }
